cc1f534d6bd3f545e08230245fb0b732d2730db4
maciusxoxo <80702535+maciusxoxo@users.noreply.github.com>
2022-01-30 17:48:06 +0100

    Delete find-perpexior.js

--- komendy/find-perpexior.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'find-perpexior.js',
-    description: 'IP PERPEXIOR',
-    execute(message, args) {
-  message.channel.send('perpexior - 89.161.94.204');
-
-    },
-};
